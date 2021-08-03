a9c0ac1d54d7bcd7e057d884073e99f81145197f
Sharrod Hines <sharrodhines@gmail.com>
2021-08-03 00:54:37 -0400

    initial commit with search form set up

---
+++ src/components/SearchMovies/index.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+
+const SearchMovie = () => {
+  const [query, setQuery] = useState('');
+
+  const getMovies = async (e) => {
+    e.preventDefault();
+    
+    const url = `https://api.themoviedb.org/3/movie/550?api_key=86cad986d0f8dab2d07c7fce77767edf&language=en-US&query=${query}&page=1&include_adult=false`;
+    
+    try {
+       // await is used because a promise is returned
+      const res = await fetch(url);
+      const data  = await res.json();
+      console.log(data);
+    } catch(err){
+        console.error(err);
+    }
+  
+  }
+
+  return (
+    <form className="form" onSubmit={getMovies}>
+      <label htmlFor="query" className="label">Movie Name</label>
+      <input className="input" 
+            type="text" 
+            name="query" 
+            placeholder="Enter a movie name"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)} />
+      <button className="button" type="submit">Search</button>
+    </form>
+  )
+ 
+}
+
+export default SearchMovie;
