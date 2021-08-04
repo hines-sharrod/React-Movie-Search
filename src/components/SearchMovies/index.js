f507da3dd262821984505f0a4c22bc8e8f04d982
Sharrod Hines <sharrodhines@gmail.com>
2021-08-03 23:18:38 -0400

    Update card styling

--- src/components/SearchMovies/index.js
+++ src/components/SearchMovies/index.js
@@ -14,7 +14,6 @@ const SearchMovie = () => {
       const res = await fetch(url);
       const data = await res.json();
       setMovies(data.results);
-      console.log(movies);
     } catch (err) {
       console.error(err);
     }
@@ -39,246 +38,47 @@ const SearchMovie = () => {
         </button>
       </form>
 
-      <div className="card">
-        <div className="card_left">
-          <img
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/343086/h8fnwL1.png"
-            alt="movie poster"
-          />
-        </div>
-        <div className="card_right">
-          <h1>KILL BILL: VOL. 1</h1>
-          <div className="card_right__details">
-            <ul>
-              <li>2003</li>
-              <li>111 min</li>
-              <li>Action</li>
-            </ul>
-            <div className="card_right__rating">
-              <div className="card_right__rating__stars">
-                <fieldset className="rating">
-                  <input id="star10" name="rating" type="radio" value="10" />
-                  <label
-                    className="full"
-                    htmlFor="star10"
-                    title="10 stars"
-                  ></label>
-
-                  <input
-                    id="star9half"
-                    name="rating"
-                    type="radio"
-                    value="9 and a half"
-                  />
-                  <label
-                    className="half"
-                    htmlFor="star9half"
-                    title="9.5 stars"
-                  ></label>
-
-                  <input id="star9" name="rating" type="radio" value="9" />
-                  <label
-                    className="full"
-                    htmlFor="star9"
-                    title="9 stars"
-                  ></label>
-
-                  <input
-                    id="star8half"
-                    name="rating"
-                    type="radio"
-                    value="8 and a half"
-                  />
-                  <label
-                    className="half"
-                    htmlFor="star8half"
-                    title="8.5 stars"
-                  ></label>
-
-                  <input id="star8" name="rating" type="radio" value="8" />
-                  <label
-                    className="full"
-                    htmlFor="star8"
-                    title="8 stars"
-                  ></label>
-
-                  <input
-                    id="star7half"
-                    name="rating"
-                    type="radio"
-                    value="7 and a half"
-                  />
-                  <label
-                    className="half"
-                    htmlFor="star7half"
-                    title="7.5 stars"
-                  ></label>
-
-                  <input id="star7" name="rating" type="radio" value="7" />
-                  <label
-                    className="full"
-                    htmlFor="star7"
-                    title="7 stars"
-                  ></label>
-
-                  <input
-                    id="star6half"
-                    name="rating"
-                    type="radio"
-                    value="6 and a half"
-                  />
-                  <label
-                    className="half"
-                    htmlFor="star6half"
-                    title="6.5 stars"
-                  ></label>
-
-                  <input id="star6" name="rating" type="radio" value="6" />
-                  <label
-                    className="full"
-                    htmlFor="star6"
-                    title="6 star"
-                  ></label>
-
-                  <input
-                    id="star5half"
-                    name="rating"
-                    type="radio"
-                    value="5 and a half"
-                  />
-                  <label
-                    className="half"
-                    htmlFor="star5half"
-                    title="5.5 stars"
-                  ></label>
-
-                  <input id="star5" name="rating" type="radio" value="5" />
-                  <label
-                    className="full"
-                    htmlFor="star5"
-                    title="5 stars"
-                  ></label>
-
-                  <input
-                    id="star4half"
-                    name="rating"
-                    type="radio"
-                    value="4 and a half"
-                  />
-                  <label
-                    className="half"
-                    htmlFor="star4half"
-                    title="4.5 stars"
-                  ></label>
-
-                  <input id="star4" name="rating" type="radio" value="4" />
-                  <label
-                    className="full"
-                    htmlFor="star4"
-                    title="4 stars"
-                  ></label>
-
-                  <input
-                    id="star3half"
-                    name="rating"
-                    type="radio"
-                    value="3 and a half"
-                  />
-                  <label
-                    className="half"
-                    htmlFor="star3half"
-                    title="3.5 stars"
-                  ></label>
-
-                  <input id="star3" name="rating" type="radio" value="3" />
-                  <label
-                    className="full"
-                    htmlFor="star3"
-                    title="3 stars"
-                  ></label>
-
-                  <input
-                    id="star2half"
-                    name="rating"
-                    type="radio"
-                    value="2 and a half"
-                  />
-                  <label
-                    className="half"
-                    htmlFor="star2half"
-                    title="2.5 stars"
-                  ></label>
-
-                  <input id="star2" name="rating" type="radio" value="2" />
-                  <label
-                    className="full"
-                    htmlFor="star2"
-                    title="2 stars"
-                  ></label>
-
-                  <input
-                    id="star1half"
-                    name="rating"
-                    type="radio"
-                    value="1 and a half"
-                  />
-                  <label
-                    className="half"
-                    htmlFor="star1half"
-                    title="1.5 stars"
-                  ></label>
-
-                  <input id="star1" name="rating" type="radio" value="1" />
-                  <label
-                    className="full"
-                    htmlFor="star1"
-                    title="1 star"
-                  ></label>
-
-                  <input
-                    id="starhalf"
-                    name="rating"
-                    type="radio"
-                    value="half"
-                  />
-                  <label
-                    className="half"
-                    htmlFor="starhalf"
-                    title="0.5 stars"
-                  ></label>
-                </fieldset>
+      {movies.map((movie) => {
+        return (
+          <div className="card" key={movie.id}>
+            <div className="card_left">
+              <img
+                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                alt={movie.title + " movie poster"}
+              />
+            </div>
+            <div className="card_right">
+              <h1>{movie.title}</h1>
+              <div className="card_right__details">
+                <ul>
+                  <li>{movie.release_date ? new Date(movie.release_date).getFullYear() : ''}</li>
+                  <li>111 min</li>
+                  <li>Action</li>
+                </ul>
+                <div className="card_right__review">
+                  <p>{movie.overview.substring(0,200) + '...'}</p>
+                  <a
+                    href="https://www.imdb.com/title/tt0266697/plotsummary?ref_=tt_stry_pl"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Read more
+                  </a>
+                </div>
+                <div className="card_right__button">
+                  <a
+                    href="https://www.youtube.com/watch?v=ot6C1ZKyiME"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    WATCH TRAILER
+                  </a>
+                </div>
               </div>
             </div>
-            <div className="card_right__review">
-              <p>
-                The lead character, called 'The Bride,' was a member <br /> of
-                the Deadly Viper Assassination Squad, led by her <br /> lover
-                'Bill.' Upon realizing she was pregnant with Bill's
-                <br /> child, 'The Bride' decided to escape her life as a
-                killer.
-                <br /> She fled to Texas, met a young man, who, on the day
-                <br /> of their wedding rehearsal was gunned down by....
-              </p>
-              <a
-                href="https://www.imdb.com/title/tt0266697/plotsummary?ref_=tt_stry_pl"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Read more
-              </a>
-            </div>
-            <div className="card_right__button">
-              <a
-                href="https://www.youtube.com/watch?v=ot6C1ZKyiME"
-                target="_blank"
-                rel="noreferrer"
-              >
-                WATCH TRAILER
-              </a>
-            </div>
           </div>
-        </div>
-      </div>
+        );
+      })}
     </>
   );
 };
